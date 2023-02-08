ba8bfd486a175b0101bf6dd4aa7adc593ce7df8e
TheSmith <43218493+thesmithdao@users.noreply.github.com>
2023-02-08 01:58:28 -0300

    Update foxwhale_1.0.1.py
    full code review, added try-except block to handle "tweepy.errors.Forbidden: 403 Forbidden exception" which will occur if the API call to update the status fails due to some reason such as a duplicate status update.

--- foxwhale_1.0.1.py
+++ foxwhale_1.0.1.py
@@ -3,10 +3,10 @@ import schedule
 import time
 import tweepy
 
-consumer_key = "[your-key]"
-consumer_secret = "[your-secret]"
-access_token = "[your-key]"
-access_token_secret = "[your-secret]"
+consumer_key = "[YOUR-KEY]"
+consumer_secret = "[YOUR-KEY]"
+access_token = "[YOUR-KEY]"
+access_token_secret = "[YOUR-KEY]"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -16,7 +16,7 @@ def fetch_data():
     query = """
     {
         transfers(
-            first: 5
+            first: 20
             orderBy:blockNumber
             orderDirection:desc
         ) {
@@ -46,17 +46,20 @@ def fetch_data():
     tweet_count = 0
 
     for transfer in transfers:
-        if int(transfer['value']) >= 99999999999999999999999:
-            transfer_value = "{:,.2f}".format(int(float(transfer['value'])) / 10**18)
+        if int(transfer['value']) >= 500000000000000000000000:
+            transfer_value = "{:,.2f}".format(int(transfer['value']) / 10**18)
             tweet_text = f"🚨🐋 Whale Alert! A transfer of {transfer_value} $FOX 🦊 was made from {transfer['from'][:4]}...{transfer['from'][-3:]} to {transfer['to'][:4]}...{transfer['to'][-3:]}. https://etherscan.com/tx/{transfer['transactionHash']} 🤖"
-            api.update_status(tweet_text)
-            tweet_count += 1
-        if tweet_count >= 2:
-            return
+            try:
+                api.update_status(tweet_text)
+                tweet_count += 1
+            except tweepy.errors.Forbidden:
+                print("Error: Tweet is a duplicate.")
+            if tweet_count >= 2:
+                return
 
 schedule.every().day.at("06:00").do(fetch_data)
-schedule.every().day.at("12:00").do(fetch_data)
-schedule.every().day.at("16:20").do(fetch_data)
+schedule.every().day.at("14:00").do(fetch_data)
+schedule.every().day.at("23:00").do(fetch_data)
 
 while True:
     schedule.run_pending()
